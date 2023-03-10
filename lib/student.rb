bcccde409678c109397a00ae3b8fde9689e43199
Charbel Maroun <charbel-maroun@hotmail.com>
2023-03-10 01:40:16 -0500

    commit tree 801062c61966e89e5695e449a3deef89f92dac02

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+require_relative './user'
+require "pry"
 
+class Student < User
+    attr_reader :knowledge
+
+    def initialize
+      @knowledge = []
+    end
+    def learn(string)
+        @knowledge << string
+    end
+# binding.pry
 end
